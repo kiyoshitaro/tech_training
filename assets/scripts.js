83f14fe8e2656e17341454e5c7d37dc413442a69
brown.nguyen <brown.nguyen@avepoint.com>
2021-03-30 18:44:27 +0700

    refactor

---
+++ assets/scripts.js
@@ -0,0 +1,14 @@
+function changeTheme() {
+  let s = document.getElementById("theme").value;
+  let areas = document.getElementsByClassName("theme_class");
+  for (area of areas) {
+    h3s = area.getElementsByTagName("h3");
+    for (h3 of h3s) {
+      h3.style.color = s;
+    }
+    hrs = area.getElementsByTagName("hr");
+    for (hr of hrs) {
+      hr.style.backgroundColor = s;
+    }
+  }
+}
