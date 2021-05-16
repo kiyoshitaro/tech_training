1f81a7ba70432958cdf124a8a9088b698feabdbc
brown.nguyen <brown.nguyen@avepoint.com>
2021-05-16 19:02:11 +0700

    add webpart

---
+++ PnPSitesCoreDemo/PnPSitesCoreDemo/Modules/DeployPage.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+
+namespace PnPSitesCoreDemo.Modules
+{
+    [XmlRoot(ElementName = "PageConfig")]
+
+    public class DeployPage
+    {
+        [XmlAttribute(AttributeName = "Name")]
+        public string Name { set; get; }
+        [XmlAttribute(AttributeName = "LogoUrl")]
+        public string LogoUrl { set; get; }
+
+        [XmlElement(ElementName = "Section")] 
+        public List<Section> Sections { set; get; }
+
+    }
+
+
+    public class Section
+    {
+        [XmlAttribute(AttributeName = "SectionType")]
+        public string SectionType { get; set; }
+
+        [XmlElement("WebPart")]
+        public List<WebPart> WebParts { set; get; }
+
+    }
+    public class WebPart
+    {
+        [XmlAttribute(AttributeName = "Name")]
+        public string Name { get; set; }
+
+        [XmlAttribute(AttributeName = "ListName")]
+        public string ListName { get; set; }
+
+        [XmlAttribute(AttributeName = "PostPerPage")]
+        public int PostPerPage { get; set; }
+
+        [XmlAttribute(AttributeName = "Column")]
+        public int Column { get; set; }
+    }
+
+
+
+}
--- PnPSitesCoreDemo/PnPSitesCoreDemo/Program.cs
+++ PnPSitesCoreDemo/PnPSitesCoreDemo/Program.cs
@@ -12,26 +12,230 @@ using PnPSitesCoreDemo.Modules;
 using System.Reflection;
 using PnPSitesCoreDemo.Utility;
 using System.Diagnostics;
+using OfficeDevPnP.Core.Pages;
 
 namespace PnPSitesCoreDemo
 {
     class Program
     {
+        private static Dictionary<string, CanvasSectionTemplate> SectionTypeMapping = new Dictionary<string, CanvasSectionTemplate>() {
+             { "OneColumn", CanvasSectionTemplate.OneColumn },
+            { "TwoColumnLeft", CanvasSectionTemplate.TwoColumnLeft }
+
+        };
+
+
+        private static void DeployPage(DeployPage PageConfig, ClientContext context)
+        {
+            try
+            {
+
+                //LOGO 
+                var web = context.Web;
+                //context.Load(web, p => p.Title);
+                string SiteLogoUrl = PageConfig.LogoUrl;
+                web.SiteLogoUrl = SiteLogoUrl;
+                web.Update();
+                context.ExecuteQuery();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+            //PAGE
+            string PageName = PageConfig.Name;
+            ClientSidePage Page = null;
+            try {
+                Page = ClientSidePage.Load(context, PageName);
+
+                Console.WriteLine($"1");
+
+                Page.Publish();
+                Page.ClearPage();
+                Page.Save();
+                Page.Publish();
+
+                Console.WriteLine($"Reloaded {PageName}");
+
+
+                //SECTION
+                var SectionConfig = PageConfig.Sections;
+
+                if (SectionConfig.Count > 0)
+                {
+                    for (int i = 0; i < SectionConfig.Count; i++)
+                    {
+                        Console.WriteLine($"-----Creating section {i+1}-----");
+                        Console.WriteLine($"--------------------------------");
+
+                        string SectionType = SectionConfig[i].SectionType;
+                        CanvasSection Section = new CanvasSection(Page, SectionTypeMapping[SectionType], 0);
+                        Page.AddSection(Section);
+
+
+                        //WEBPARTS
+                        string Name = "";
+                        string ListName = "";
+                        int PostPerPage = 4;
+                        int Column = 0;
+
+                        var WebpartConfig = SectionConfig[i].WebParts;
+
+                        if (WebpartConfig.Count > 0)
+                        {
+                            for (int j = 0; j < WebpartConfig.Count; j++)
+                            {
+                                try
+                                {
+                                    Name = WebpartConfig[j].Name;
+                                    ListName = WebpartConfig[j].ListName;
+                                    PostPerPage = WebpartConfig[j].PostPerPage;
+                                    Column = WebpartConfig[j].Column;
+                                }
+                                catch (Exception e)
+                                {
+                                    Console.WriteLine(e.Message);
+                                }
+                                Console.WriteLine($"-----Creating {Name} webpart-----");
+                                var Webpart = Page.AvailableClientSideComponents(Name).ToList();
+                                string PropertiesJson = "";
+
+                                ClientSideWebPart ClientWP = null;
+                                try
+                                {
+                                    ClientWP = new ClientSideWebPart(Webpart.First());
+                                }
+                                catch (Exception e)
+                                {
+                                    Console.WriteLine(e.Message);
+
+                                }
+
+                                switch (Name)
+                                {
+                                    case "announcement":
+                                        PropertiesJson = "{\"listName\": \"" + ListName + "\"," +
+                                                            "\"postPerPage\": \"" + PostPerPage + "\"}";
+                                        break;
+                                    case "news":
+                                        PropertiesJson = "{\"listName\": \"" + ListName + "\"," +
+                                                            "\"postPerPage\": \"" + PostPerPage + "\"}";
+                                        //Console.WriteLine($"-----cccccccc-----{PropertiesJson}");
+                                        break;
+                                    default:
+                                        break;
+                                }
+                                ClientWP.PropertiesJson = PropertiesJson;
+                                Page.AddControl(ClientWP, Section.Columns[Column]);
+
+                                Console.WriteLine($"-----Created {Name} webpart-----");
+                                Console.WriteLine($"--------------------------------");
+
+                            }
+                        }
+                        else {
+                            Console.WriteLine($"No webpart to create");
+                        }
+                        Console.WriteLine($"-----Created section {i+1}-----");
+                        Console.WriteLine($"--------------------------------");
+
+                    }
+                    Page.Save();
+                    Page.Publish();
+                    Console.WriteLine($"-----Save and Publish page-----");
+
+                }
+                else {
+                    Console.WriteLine($"No section to create");
+                }
+            }
+            catch
+            {
+                Console.WriteLine($"Create new page {PageName}");
+                Page = new ClientSidePage(context, ClientSidePageLayoutType.Home);
+            }
+
+
+
+
+            //string pageName = "Test-Page.aspx";
+            //ClientSidePage newPage = null;
+            //try
+            //{
+            //    //EXISTED
+            //    newPage = ClientSidePage.Load(context, pageName);
+
+            //    //need to public first
+            //    newPage.Publish();
+            //    newPage.ClearPage();
+            //    newPage.Save();
+            //    newPage.Publish();
+
+            //    //SECTION
+            //    CanvasSection section = new CanvasSection(newPage, CanvasSectionTemplate.TwoColumn, 0);
+            //    newPage.AddSection(section);
+
+            //    //WEBPART1
+            //    ClientSideText webpart1 = new ClientSideText()
+            //    {
+            //        Text = "abc"
+            //    };
+            //    newPage.AddControl(webpart1, section.Columns[0]);
+
+
+            //    //WEBPART2               
+            //    ClientSideWebPart webpart2 = newPage.InstantiateDefaultWebPart(DefaultClientSideWebParts.List);
+            //    webpart2.Title = "document gallery";
+            //    webpart2.PropertiesJson = "{}";
+            //    newPage.AddControl(webpart2, section.Columns[1]);
+
+            //    newPage.Save();
+            //    newPage.Publish();
+            //}
+            //catch
+            //{
+            //    Console.WriteLine($"Create new page {pageName}");
+
+            //    newPage = new ClientSidePage(context, ClientSidePageLayoutType.Home);
+
+            //}
+
+        }
+
         static void Main(string[] args)
         {
+
+
             var siteUrl = ConfigurationManager.AppSettings["siteUrl"];
             var username = ConfigurationManager.AppSettings["accountName"];
             Console.WriteLine($"SharePoint Online User Name: {username}");
             Console.WriteLine("Please input user Password: ");
+
             var password = GetPassword();
             ClientContext context = new ClientContext(siteUrl);
             context.Credentials = new SharePointOnlineCredentials(username, password);
+
+
+
             var constConfig = new XmlDocument();
             constConfig.Load(@"Configuration\Configuration.xml");
-            Elements configrations = XmlUtility.ToObject<Elements>(constConfig.InnerXml);
+            Elements Configuration = XmlUtility.ToObject<Elements>(constConfig.InnerXml);
+           
+            var PageConfigXml = new XmlDocument();
+            PageConfigXml.Load(@"Configuration\PageConfig.xml");
+            Console.WriteLine(PageConfigXml.InnerXml);
+            DeployPage PageConfig = XmlUtility.ToObject<DeployPage>(PageConfigXml.InnerXml);
+
+
+
+
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
-            Deploy(configrations.DeployElements, context);
+
+            DeployPage(PageConfig, context);
+
             stopwatch.Stop();
             Console.WriteLine(string.Format("Home site deployment takes: {0} s", stopwatch.ElapsedTicks / 10000000));
             Console.ReadLine();
@@ -80,6 +284,10 @@ namespace PnPSitesCoreDemo
             {
                 using (context)
                 {
+
+                    Console.WriteLine("sssss");
+                    //System.Environment.Exit(0);
+
                     Type xmlType = configrationElemetns.GetType();
                     foreach (PropertyInfo property in xmlType.GetProperties())
                     {
