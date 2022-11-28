96db1cb48b23b1c81f6addca821d8030a29eb3ee
kiyoshitaro <hunglhp1998@gmail.com>
2022-11-28 23:51:31 +0700

    Update stack

---
+++ python/739.daily-temperatures.py
@@ -0,0 +1,58 @@
+#
+# @lc app=leetcode id=739 lang=python3
+#
+# [739] Daily Temperatures
+#
+# https://leetcode.com/problems/daily-temperatures/description/
+#
+# algorithms
+# Medium (66.59%)
+# Likes:    8687
+# Dislikes: 201
+# Total Accepted:    497K
+# Total Submissions: 746.4K
+# Testcase Example:  '[73,74,75,71,69,72,76,73]'
+#
+# Given an array of integers temperatures represents the daily temperatures,
+# return an array answer such that answer[i] is the number of days you have to
+# wait after the i^th day to get a warmer temperature. If there is no future
+# day for which this is possible, keep answer[i] == 0 instead.
+# 
+# 
+# Example 1:
+# Input: temperatures = [73,74,75,71,69,72,76,73]
+# Output: [1,1,4,2,1,1,0,0]
+# Example 2:
+# Input: temperatures = [30,40,50,60]
+# Output: [1,1,1,0]
+# Example 3:
+# Input: temperatures = [30,60,90]
+# Output: [1,1,0]
+# 
+# 
+# Constraints:
+# 
+# 
+# 1 <= temperatures.length <= 10^5
+# 30 <= temperatures[i] <= 100
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack = []
+        res = [0 for _ in range(len(temperatures))]
+        for ind in range(len(temperatures)):
+            while stack:
+                if temperatures[ind] > temperatures[stack[-1]]:
+                    index = stack.pop()
+                    res[index] += ind - index
+                else:
+                    break
+            stack.append(ind)
+        return res
+        
+# @lc code=end
+
