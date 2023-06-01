a0aa21fd1bcf0099eb1d9a2b8bab51a321e366bf
kiyoshitaro <hunglhp1998@gmail.com>
2023-06-01 23:52:50 +0700

    UPDATEzksync

---
+++ solidity/contracts/SimpleStorage.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+contract SimpleStorage {
+    uint256 public storedData; //Do not set 0 manually it wastes gas!
+
+    event setEvent();
+
+    function set(uint256 x) public {
+        storedData = x;
+        emit setEvent();
+    }
+}
---
+++ solidity/deploy/deploySimpleStorage.ts
@@ -0,0 +1,26 @@
+import * as ethers from "ethers";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { Wallet } from "zksync-web3";
+import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
+
+const privateKey = process.env.ZK_GOERLI_PRIVATE_KEY || "";
+if (!privateKey) {
+  throw "Private key not detected!";
+}
+export default async function (hre: HardhatRuntimeEnvironment) {
+  console.log(`Running deploy script for the SimpleStorage contract`);
+
+  const wallet = new Wallet(privateKey);
+
+  const deployer = new Deployer(hre, wallet);
+  const artifact = await deployer.loadArtifact("SimpleStorage");
+
+  const deploymentFee = await deployer.estimateDeployFee(artifact, []);
+  const parsedFee = ethers.utils.formatEther(deploymentFee.toString());
+  console.log(`The deployment is estimated to cost ${parsedFee} ETH`);
+
+  const SimpleStorageContract = await deployer.deploy(artifact);
+
+  const contractAddress = SimpleStorageContract.address;
+  console.log(`${artifact.contractName} was deployed to ${contractAddress}`);
+}
