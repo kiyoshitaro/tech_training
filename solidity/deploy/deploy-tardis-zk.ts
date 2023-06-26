57dd10a673392df4171c893fcc174ab4f60eabf9
kiyoshitaro <hunglhp1998@gmail.com>
2023-06-26 23:07:09 +0700

    UPDATE key and Deploy tardis

--- solidity/deploy/deploy-tardis-zk.ts
+++ solidity/deploy/deploy-tardis-zk.ts
@@ -8,19 +8,19 @@ if (!privateKey) {
   throw "Private key not detected!";
 }
 export default async function (hre: HardhatRuntimeEnvironment) {
-  console.log(`Running deploy script for the SimpleStorage contract`);
+  console.log(`Running deploy script for the Tardis token contract`);
 
   const wallet = new Wallet(privateKey);
 
   const deployer = new Deployer(hre, wallet);
-  const artifact = await deployer.loadArtifact("SimpleStorage");
+  const artifact = await deployer.loadArtifact("Tardis");
 
   const deploymentFee = await deployer.estimateDeployFee(artifact, []);
   const parsedFee = ethers.utils.formatEther(deploymentFee.toString());
   console.log(`The deployment is estimated to cost ${parsedFee} ETH`);
 
-  const SimpleStorageContract = await deployer.deploy(artifact);
+  const TardisContract = await deployer.deploy(artifact);
 
-  const contractAddress = SimpleStorageContract.address;
+  const contractAddress = TardisContract.address;
   console.log(`${artifact.contractName} was deployed to ${contractAddress}`);
 }
