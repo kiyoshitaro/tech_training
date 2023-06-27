4eb9fc8531dec2a6b1d240b6725d645ed84f7168
kiyoshitaro <hunglhp1998@gmail.com>
2023-06-28 00:49:04 +0700

    UPDATE query block

--- etherjs_and_web3js/constant.ts
+++ etherjs_and_web3js/constant.ts
@@ -8,6 +8,7 @@ export const MY_ADDRESS_2 = '0xbC278D6583b97399014F3B3c64D295135660C629';
 export const zk_provider = new ethers.providers.JsonRpcProvider(ZKSYNC_TESTNET_PROVIDER)
 export const TDS_TOKEN_ADDRESS_ZK_TESTNET = '0x09825c8C53Ac725A526360a33Cb0260e83c598F5';
 export const USDC_TOKEN_ADDRESS_ZK_TESTNET = '0x0faF6df7054946141266420b43783387A78d82A9'
+export const ZKUSDC_TOKEN_ADDRESS_ZK_TESTNET = '0xA4D8FECEf6C4f77bDe74eDBc7931de2e47828B3F'
 export const myWallet = new ethers.Wallet(process.env.PRIVATE_KEY, zk_provider)
 export const getContract = (contractAddress: string, abi: any) => {
   return new ethers.Contract(contractAddress, abi, zk_provider);
