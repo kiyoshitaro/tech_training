11d771ab6a164f30f6f5968e816cb8f2be433f0a
kiyoshitaro <hunglhp1998@gmail.com>
2023-07-10 23:40:54 +0700

    UPDATE eth provider

--- etherjs_and_web3js/constant.ts
+++ etherjs_and_web3js/constant.ts
@@ -4,11 +4,13 @@ import * as dotenv from "dotenv";
 dotenv.config({ path: __dirname + '/.env' });
 
 export const ZKSYNC_TESTNET_PROVIDER = 'https://testnet.era.zksync.dev';
+export const ETH_GOERLI_TESTNET_PROVIDER = 'https://eth-goerli.g.alchemy.com/v2/demo';
 export const MY_ADDRESS = '0xf9F689367990f981BCD267FB1A4c45f63B6Bd7b1';
 export const MY_ADDRESS_2 = '0xbC278D6583b97399014F3B3c64D295135660C629';
 export const MY_ADDRESS_3 = '0xd40A929027c04CEecf78E034b7F828CF999EEC79';
 export const MY_ADDRESS_4 = '0x6Ff7A794182D94d33bfcdEc7324fa16DA73F9db4';
 export const zk_provider = new ethers.providers.JsonRpcProvider(ZKSYNC_TESTNET_PROVIDER)
+export const eth_provider = new ethers.providers.JsonRpcProvider(ETH_GOERLI_TESTNET_PROVIDER)
 export const TDS_TOKEN_ADDRESS_ZK_TESTNET = '0x09825c8C53Ac725A526360a33Cb0260e83c598F5';
 export const USDC_TOKEN_ADDRESS_ZK_TESTNET = '0x0faF6df7054946141266420b43783387A78d82A9'
 export const ZKUSDC_TOKEN_ADDRESS_ZK_TESTNET = '0xA4D8FECEf6C4f77bDe74eDBc7931de2e47828B3F'
