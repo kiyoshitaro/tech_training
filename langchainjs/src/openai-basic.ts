2148c1c02ad646e84d8d2c7a058bec42320dbbe0
kiyoshitaro <hunglhp1998@gmail.com>
2024-05-20 18:55:26 +0700

    UPDATE crawl chart

--- langchainjs/src/openai-basic.ts
+++ langchainjs/src/openai-basic.ts
@@ -22,21 +22,20 @@ const toDataUrl = (url: any, callback: any) => {
 }
 
 (async () => {
-  var imageAsBase64 = fs.readFileSync(path.join(__dirname, '../assets/laso.png'), 'base64');
-
+  let imageAsBase64 = fs.readFileSync(path.join(__dirname, '../assets/dexscreener.com_DCM_WETH_2024-05-20_17-30-08.png'), 'base64');
   const openai = new OpenAI({
-    // apiKey: openAIKey,
-    baseURL: 'http://localhost:1234/v1'
+    apiKey: openAIKey,
+    // baseURL: 'http://localhost:1234/v1'
   });
   const completion = await openai.chat.completions.create({
     messages: [
-      { role: "system", content: "Bạn có kiến thức về tử vi." },
+      { role: "system", content: "Bạn có kiến thức phân tích chart, hãy phân tích điểm entry point và take profit cho tôi dựa vào các chỉ số macd, rsi, ema" },
       // { role: "user", content: "Who are you?" },
       {
         role: "user", content: [
           {
             "type": "text",
-            "text": "Mô tả lá số sau đây",
+            "text": "phân tích điểm entry point và take profit của chart sau",
           },
           {
             "type": "image_url",
