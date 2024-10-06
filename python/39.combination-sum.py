12f52db72ee08f563f804fa9e60bb270b00db539
kiyoshitaro <hunglhp1998@gmail.com>
2024-10-06 23:57:51 +0700

    UPDATE algo

--- python/39.combination-sum.py
+++ python/39.combination-sum.py
@@ -66,18 +66,18 @@
 # @lc code=start
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        res = []
-        def backtrack(i, subset, sum):
+        res, subset = [], []
+        def backtrack(i, sum):
             if(sum == target):
                 res.append(subset.copy())
                 return
             if(sum > target or i >= len(candidates)):
                 return
             subset.append(candidates[i])
-            backtrack(i , subset, sum + candidates[i])
+            backtrack(i , sum + candidates[i])
             subset.pop()
-            backtrack(i +1 , subset, sum)
-        backtrack(0,[], 0)
+            backtrack(i +1 , sum)
+        backtrack(0, 0)
         return res
             
 # @lc code=end
