ad952844c789247105d4fbaafb5cb6e026a41f01
kiyoshitaro <hunglhp1998@gmail.com>
2026-02-24 18:15:31 +0700

    update mmap

--- golang/cmd/wc/main.go
+++ golang/cmd/wc/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"io"
 	"log"
 	"os"
+
+	"golang.org/x/exp/mmap"
 
 	// "runtime"
 	// "runtime/pprof"
@@ -15,10 +16,11 @@ import (
 )
 
 var buff [1]byte
+
 func readbyte(f io.Reader) (rune, error) {
-	// (do params truyền vào gắn với buff là interface (io.Reader), 
-	// interface trong Go có thể trỏ đến bất kỳ kiểu nào (file, buffer,..). compiler và runtime không biết 
-	// chắc bên trong Reader sẽ làm gì với dữ liệu bạn đưa nên buff sẽ 
+	// (do params truyền vào gắn với buff là interface (io.Reader),
+	// interface trong Go có thể trỏ đến bất kỳ kiểu nào (file, buffer,..). compiler và runtime không biết
+	// chắc bên trong Reader sẽ làm gì với dữ liệu bạn đưa nên buff sẽ
 	// được heap allocated cẩn thận theo escape analysis (sẽ đưa hết cả file lên heap) -> GC phải dọn
 	// )   => khai báo global để tái sử dụng một buffer duy nhất
 	// var buff [1]byte
@@ -27,25 +29,19 @@ func readbyte(f io.Reader) (rune, error) {
 }
 
 func main() {
-	// Kích hoạt CPU pprof, OS sẽ thiết lập một interrupt (~100 tps) và lấy sample các call stack đang hoạt động->overhead, 
+	// Kích hoạt CPU pprof, OS sẽ thiết lập một interrupt (~100 tps) và lấy sample các call stack đang hoạt động->overhead,
 	// hàm nào xuất hiện nhiều nhất trong các sample đó là những hàm đang tiêu tốn nhiều CPU nhất.
 	// defer profile.Start(profile.CPUProfile, profile.ProfilePath(".")).Stop()
-	// Do chính Go Runtime tự ghi lại. Mỗi khi chương trình yêu cầu allocation, 
+
+	// Do chính Go Runtime tự ghi lại. Mỗi khi chương trình yêu cầu allocation,
 	// Go Runtime sẽ lưu lại stack trace => overhead thấp hơn.
-	// defer profile.Start(profile.MemProfile, profile.MemProfileRate(1), profile.ProfilePath(".")).Stop()
-	defer profile.Start(profile.TraceProfile,  profile.ProfilePath(".")).Stop()
+	defer profile.Start(profile.MemProfile, profile.MemProfileRate(1), profile.ProfilePath(".")).Stop()
 
-	// fcpu, _ := os.Create("cpu.pprof")
-	// pprof.StartCPUProfile(fcpu)
-	// defer pprof.StopCPUProfile()
-	// runtime.MemProfileRate = 1 * 1024 // Mỗi 64KB allocated sẽ ghi lại một sample vào memory profile
-	// fmem, _ := os.Create("mem.pprof")
-	// defer func() {
-	// 	pprof.WriteHeapProfile(fmem)
-	// 	fmem.Close()
-	// }()
+	// trace ghi lại mọi sự kiện xảy ra trong Runtime: khi nào Goroutine chạy, khi nào dừng, chạy trên CPU nào, phân phối biến động thế nào 
+	// defer profile.Start(profile.TraceProfile,  profile.ProfilePath(".")).Stop()
 
-	f, err := os.Open(os.Args[1])
+	// f, err := os.Open(os.Args[1])
+	f, err := mmap.Open(os.Args[1])
 	if err != nil {
 		log.Fatalf("could not open file %q: %v", os.Args[1], err)
 	}
@@ -54,18 +50,26 @@ func main() {
 	words := 0
 	inword := false
 	// cpu profiler thấy lượng lớn CPU time nằm ở hàm syscall, nhưng syscall không chậm, do chương trình đang gọi quá nhiều lần
-	// Trong Go, các thao tác read/write mặc định là unbuffered. 
+	// Trong Go, các thao tác read/write mặc định là unbuffered.
 	// 1. Nếu đọc 1 file lớn từng byte một, mỗi byte sẽ tạo ra một syscall và chuyển user-kernel liên tục -> overhead lớn
 	// 2. Goroutine thực hiện syscall mà không xong sớm, Runtime sẽ handoff gây thêm đổi luồng liên tục
-	b := bufio.NewReader(f)
-	for {
-		r, err := readbyte(b)
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatalf("could not read file %q: %v", os.Args[1], err)
-		}
+	// b := bufio.NewReader(f)
+	// for {
+	// 	r, err := readbyte(b)
+	// 	if err == io.EOF {
+	// 		break
+	// 	}
+	// 	if err != nil {
+	// 		log.Fatalf("could not read file %q: %v", os.Args[1], err)
+	// 	}
+
+	// Dùng mmap để ánh xạ toàn bộ file vào không gian địa chỉ bộ nhớ của tiến trình -> truy cập file như truy cập mảng trong RAM
+	// CPU Không cần syscall đọc file liên tục -> Không cần copy từ kernel sang user,
+	// vì file đã được ánh xạ trực tiếp vào bộ nhớ của tiến trình (zero copy) 
+	// & chỉ những phần truy cập mới thực sự được nạp vào RAM (do cơ chế demand paging).
+	for i := 0; i < int(f.Len()); i++ {
+		r := rune(f.At(i))
+
 		if unicode.IsSpace(r) && inword {
 			words++
 			inword = false
@@ -76,9 +80,10 @@ func main() {
 	}
 	fmt.Printf("%q: %d words\n", os.Args[1], words)
 }
+
 // go build
 // time ./wc big.txt
 // go tool pprof -http=:8080 mem.pprof
-// -> CPU Profiler (detect syscall slow -> buffer) 
-// -> Memory Profiler(detect memloc consumes mem -> heap escape -> global var) 
+// -> CPU Profiler (detect syscall slow -> buffer)
+// -> Memory Profiler(detect memloc consumes mem -> heap escape -> global var)
 // -> Trace Profiler (detect single thread, goroutine scatter -> group task, goroutine not stable in thread -> worker pool, ...)
--- golang/go.mod
+++ golang/go.mod
@@ -1,9 +1,11 @@
 module go_tut
 
-go 1.21.5
+go 1.25.0
 
 require (
 	github.com/felixge/fgprof v0.9.3 // indirect
 	github.com/google/pprof v0.0.0-20211214055906-6f57359322fd // indirect
 	github.com/pkg/profile v1.7.0 // indirect
+	github.com/varstr/uaparser v0.0.0-20170929040706-6aabb7c4e98c // indirect
+	golang.org/x/exp v0.0.0-20260218203240-3dfff04db8fa // indirect
 )
